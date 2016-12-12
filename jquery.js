10aea8c5a58b9bc4e7c84a63eff124730efcc19a
fristonio <deepshpathak@gmail.com>
2016-12-12 23:51:01 +0530

    updated the loding gif

--- jquery.js
+++ jquery.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {  
 $(window).load(function(){
-	$('#preloader').delay(500).fadeOut(400,function(){$(this).remove();});
+	$('#preloader').delay(1000).slideUp(400,function(){$(this).remove();});
 });
 
 });
